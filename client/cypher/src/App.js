87690549a9d3299d1ee4e5e00722c6a7a3321c17
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-10-15 02:11:16 +0530

    navbar added

---
+++ client/cypher/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavbarBase from "./components/navbar/navbarbase";
+
+function App() {
+  return <NavbarBase />;
+}
+
+export default App;
---
+++ client/cypher/src/components/constant/navitems_text.js
@@ -0,0 +1,30 @@
+const Infromation = [
+  {
+    name: "For him",
+  },
+  {
+    name: "For her",
+  },
+  {
+    name: "Objects",
+  },
+  {
+    name: "LookBooks",
+  },
+];
+const Information2= [
+    {
+        name: "About",
+      },
+      {
+        name: "Search",
+      },
+      {
+        name: "Saved",
+      },
+    
+      {
+        name: "Card",
+      },
+]
+export { Infromation ,Information2};
