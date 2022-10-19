b0fdbce59153917f26e2b96a4c22cde990a4195a
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-10-19 09:04:30 +0530

    package .json file chnaged

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT,(req,res) => {
     console.log('listening on port: '+ PORT);
 });
+console.log(path.join(__dirname, '..','public'))
+app.use(express.static(path.join(__dirname,'..','public')))
