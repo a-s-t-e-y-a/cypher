9474a9ea27bbc8d000e5346b45fd7ab27961be75
a-s-t-e-y-a <krishnoit100@gmail.com>
2022-10-24 02:31:27 +0530

    login api setup with jwt token for authorisation

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const mongoose  = require('mongoose');
+const path = require("path");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/admin',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-)
+mongoose.connect("mongodb://localhost:27017/admin", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
@@ -16,12 +14,11 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-
-const loginRouter = require('./routes/login/login.route');
-const signupRouter  = require('./routes/signup/signup.route');
+const loginRouter = require("./routes/login/login.route");
+const signupRouter = require("./routes/signup/signup.route");
 app.use(express.json());
-console.log(path.join(__dirname, '..','public'))
-app.use(express.static(path.join(__dirname,'..','public')))
-app.use(loginRouter)
-app.use(signupRouter)
+console.log(path.join(__dirname, "..", "public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(loginRouter);
+app.use(signupRouter);
 module.exports = app;
