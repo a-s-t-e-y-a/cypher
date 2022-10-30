e96b50e0a30e6a07b6df58396cc7e280f102e835
adityajha0001 <rockadi2407@gmail.com>
2022-10-31 01:07:13 +0530

    data needed

--- client/src/components/pagination/Posts.js
+++ client/src/components/pagination/Posts.js
@@ -6,7 +6,7 @@ const Posts = ({ posts, loading }) => {
   }
 
   return (
-    <ul className='list-group mb-4'>
+    <ul className='list-group mb-24 '>
       {posts.map(post => (
         <li key={post.id} className='list-group-item'>
           {post.title}
