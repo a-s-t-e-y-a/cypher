21a9fbc39af13397354a5b4694ce2330efbbf01e
adityajha0001 <rockadi2407@gmail.com>
2022-11-04 03:45:23 +0530

    d

--- client/src/components/ItemPage.js
+++ client/src/components/ItemPage.js
@@ -2,59 +2,66 @@ function ITEMPAGE() {
     return (
         <>
             <div>
-                <div className="flex">
-                    <img src="" />
-                    <div className="pl-32">
-                        <h1 className="font-bold text-3xl ">Modern sport shoes</h1>
-                        <div className="flex">
-                            <button className="text-orange-500 bg-orange-200 rounded-lg">Trainers</button>
-                            <button className="text-green-500 bg-green-200 rounded-lg"> Woman</button>
+                <div className="flex justify-center px-32 py-32">
+                    <img className="pr-32 mr-32" style={{ height: 700 }} src="/images/girlll.jpg" />
+                    <div className="pl-12 pt-8">
+                        <h1 className="font-bold text-6xl ">Modern sport shoes</h1>
+                        <div className="flex gap-4 py-16">
+                            <button className="text-white text-4xl bg-purple-500 hover:bg-purple-700 rounded-full px-4 py-2">Trainers</button>
+                            <button className="text-white text-4xl bg-purple-500 hover:bg-purple-700 rounded-full px-4 py-2"> Woman</button>
                         </div>
-                        <p className="font-bold text-xl">$2345</p>
-                        <div className="flex">
-                            <span className="font-bold">Size</span>
-                            <span className="itelic">36,37,38,39</span>
+                        <p className="font-bold text-2xl">$2345</p>
+                        <div className="flex pt-8 gap-4">
+                            <span className="font-bold text-2xl  border-2 ...">Size</span>
+                            <span className="itelic text-2xl mx-2 border-2 ...">36,37,38,39</span>
                         </div>
-                        <div className="flex gap-2">
-                            <p>1</p>
-                            <button className="rounded-lg bg-green-300 text-white font-bold"> Add to cart</button>
+                        <div className="py-16">
+                            <button className="rounded-lg text-white text-4xl bg-purple-500 hover:bg-purple-700 px-4 py-2"> Add to cart</button>
                         </div>
                     </div>
                 </div>
 
 
-                <div className="flex justify-between py-12">
-                            <h1 className="text-2xl font-bold">About this product</h1>
-                        </div>
+                <div className="flex justify-center py-12">
+                    <h1 className="text-6xl font-bold">About this product</h1>
+                </div>
 
-                        <div className="px-16 py-32">
-                            <p>Your audience and writing purpose will determine your writing style. The four main types of writing styles are persuasive, narrative, expository, and descriptive.  In this blog post, we’ll briefly explore the defining features of these four writing styles. For more help using these writing styles, schedule an appointment at the GWC!
+                <div className="px-16 py-12">
+                    <p className="px-32">Your audience and writing purpose will determine your writing style. The four main types of writing styles are persuasive, narrative, expository, and descriptive.  In this blog post, we’ll briefly explore the defining features of these four writing styles. For more help using these writing styles, schedule an appointment at the GWC!
 
-                                Persuasive:  For this writing style, the writer is trying to convince the reader of the validity of a certain position or argument. Persuasive writing includes the writers’ opinions, and provides justifications and evidence to support their claims.
+                        Persuasive:  For this writing style, the writer is trying to convince the reader of the validity of a certain position or argument. Persuasive writing includes the writers’ opinions, and provides justifications and evidence to support their claims.
 
-                                Examples: Letters of recommendation; cover letters; Op-Eds and Editorial newspaper articles; argumentative essays for academic papers
-                            </p>
+                        Examples: Letters of recommendation; cover letters; Op-Eds and Editorial newspaper articles; argumentative essays for academic papers. Expository: This type of writing is used to explain a concept and share information to a broader audience. Expository writing provides evidence, statistics, or results and focuses on the facts of a certain topic. This type is not meant to express opinions.
 
+                        Examples: How-to articles; textbooks; news stories (not editorials or Op-Eds); business, technical, or scientific writing
 
-                        </div>
+                        Descriptive: This type of writing is used to depict imagery to create a clear picture in the mind of the reader. This method helps the readers become more connected to the writing by appealing to their senses. Descriptive writing employs literary techniques such as similes, metaphors, allegory, etc to engage the audience.
 
-
-
-
+                        Examples: Poetry; fictional novels or plays; memoirs or first-hand accounts of events
 
 
+                    </p>
 
-                    </div>
 
-
-                </>
+                </div>
 
 
 
-                )
+
+
+
+
+            </div>
+
+
+        </>
+
+
+
+    )
 }
 
 
 
 
-                export default ITEMPAGE;
+export default ITEMPAGE;
